f54e6b4b91b6fe1d1d9e1709627fa7099dc24130
jxtsy <aj8984524@gmail.com>
2024-10-17 19:16:45 -0600

    Initial commit

---
+++ env/dev/main.tf
@@ -0,0 +1,26 @@
+module "dev_vm" {
+  source                   = "../../modules/vm"
+  environment              = "dev"
+  mail_secret_key          = var.MAIL_SECRET_KEY
+  mail_user                = var.MAIL_USER
+  admin_username           = "adminuser"
+  domain                   = var.DOMAIN
+  resource_group           = "IN-RG-JaramilloT"
+  nic_name                 = "IN-NIC-JaramilloT"
+  mail_service             = var.MAIL_SERVICE
+  security_group_name      = "IN-SG-JaramilloT"
+  ssh_key_path             = "./keys/712incident_server"
+  port                     = var.PORT
+  server_name              = "IN-Server-JaramilloT" 
+  location                 = "eastus2"
+  mapbox_access_token      = var.MAPBOX_ACCESS_TOKEN
+  mongo_url                = var.MONGO_URL
+  subnet_name              = "IN-SUBNET-JaramilloT"
+  mongo_init_root_username = var.MONGO_INITDB_ROOT_USERNAME
+  mongo_init_root_password = var.MONGO_INITDB_ROOT_PASSWORD
+  mongo_db                 = var.MONGO_DB
+  ip_name                  = "IN-IP-JaramilloT"
+  vnet_name                = "IN-VNET-JaramilloT"
+}
+
+
---
+++ env/dev/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstateRGJaramillo"
+    storage_account_name = "tfstatejaramillo"
+    container_name       = "jaramillojxtsy"
+    key                  = "terraform.state"
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
