508a94025eb58937b9dcb5fb71dc9defe14932de
jxtsy <aj8984524@gmail.com>
2024-10-17 19:43:00 -0600

    Docker-compose

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -14,7 +14,7 @@ module "dev_vm" {
   server_name              = "IN-Server-JaramilloT" 
   location                 = "eastus2"
   mapbox_access_token      = var.MAPBOX_ACCESS_TOKEN
-  mongo_url                = var.MONGO_URL
+  mongo_url                = var.MONGO_URL 
   subnet_name              = "IN-SUBNET-JaramilloT"
   mongo_init_root_username = var.MONGO_INITDB_ROOT_USERNAME
   mongo_init_root_password = var.MONGO_INITDB_ROOT_PASSWORD
